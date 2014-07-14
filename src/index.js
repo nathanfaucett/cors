445b607fa10568879ab3b7c80cb5ea48fcacdb7c
lonewolfgames <nathanfaucett@gmail.com>
2014-07-14 08:34:01 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+function Cors(opts) {
+    opts || (opts = {});
+
+    this.origin = typeof(opts.origin) === "string" ? opts.origin : "*";
+    this.methods = typeof(opts.methods) === "string" ? opts.methods : (opts.methods && opts.methods.length ? opts.methods.join(",") : "GET,POST,PUT,PATCH,HEAD,DELETE");
+    this.credentials = !!opts.credentials ? "true" : "false";
+    this.allowedHeaders = typeof(opts.allowedHeaders) === "string" ? opts.allowedHeaders : (opts.allowedHeaders && opts.allowedHeaders.length ? opts.allowedHeaders.join(",") : null);
+    this.exposedHeaders = typeof(opts.exposedHeaders) === "string" ? opts.exposedHeaders : (opts.exposedHeaders && opts.exposedHeaders.length ? opts.exposedHeaders.join(",") : null);
+    this.maxAge = !!opts.maxAge ? opts.maxAge.toString() : null;
+}
+
+Cors.prototype.middleware = function(req, res, next) {
+    var headers = this.allowedHeaders || req.headers["access-control-request-headers"];
+
+    if (req.method === "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Origin", this.origin);
+        res.setHeader("Access-Control-Allow-Methods", this.methods);
+        res.setHeader("Access-Control-Allow-Credentials", this.credentials);
+        if (headers) res.setHeader("Access-Control-Allow-Headers", headers);
+        if (this.exposedHeaders) res.setHeader("Access-Control-Expose-Headers", this.exposedHeaders);
+        if (this.maxAge) res.setHeader("Access-Control-Max-Age", this.maxAge);
+        res.statusCode = 204;
+        res.end();
+    } else {
+        res.setHeader("Access-Control-Allow-Origin", this.origin);
+        res.setHeader("Access-Control-Allow-Credentials", this.credentials);
+        if (this.exposedHeaders) res.setHeader("Access-Control-Expose-Headers", this.exposedHeaders);
+    }
+
+    next();
+};
+
+
+module.exports = Cors;
